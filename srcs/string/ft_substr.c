e51648e6afd3c35e0eee5cc98a007fac5ba7d40e
jules <julesp415@gmail.com>
2020-10-17 17:17:49 +0200

    fixed substr thus strtrim aswell

--- srcs/string/ft_substr.c
+++ srcs/string/ft_substr.c
@@ -3,13 +3,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*cpy;
+	size_t	i;
 
-	if (!s || len <= 0 || start >= ft_strlen(s))
+	if (!s || start > ft_strlen(s))
 		return (NULL);
-	cpy = ft_strnew(len);
-	if (!cpy)
+	i = 0;
+	s += start;
+	while (s[i] && i < len)
+		i++;
+	if (!(cpy = ft_strnew(i)))
 		return (NULL);
-	if (start >= ft_strlen(s))
-		return ("");
-	return (ft_strncpy(cpy, (char*)s + start, len));
+	i = -1;
+	while (++i < len && s[i])
+		cpy[i] = s[i];
+	cpy[i] = 0;
+	return (cpy);
 }
