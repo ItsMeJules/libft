8c91155fba08940c3757abef8d09c625cb3fd70d
jules <julesp415@gmail.com>
2020-11-19 20:30:31 +0100

    adapted everything to the moulinette

--- Makefile
+++ Makefile
@@ -13,59 +13,45 @@ FD_SRCS_PATH	= fd
 BONUS_SRCS_PATH	= bonus
 SRCS_PATH		= srcs
 INCS_PATH		= includes
+INCS_NAME		= libft.h
 
-STR_SRCS_NAME	= $(addprefix $(STR_SRCS_PATH)/, ft_strcpy.c \
-					ft_strdel.c \
-					ft_strdup.c \
-					ft_strequ.c \
+INCS			= $(addprefix $(INCS_PATH)/, $(INCS_NAME))
+SRCS			= $(addprefix $(SRCS_PATH)/, ft_strdup.c \
 					ft_strlen.c \
-					ft_strncpy.c \
-					ft_strnequ.c \
-					ft_strnew.c \
 					ft_substr.c \
 					ft_strtrim.c \
 					ft_strchr.c \
 					ft_strrchr.c \
 					ft_strncmp.c \
 					ft_strnstr.c \
-					ft_strclr.c \
-					ft_striter.c \
-					ft_striteri.c \
-					ft_strmap.c \
 					ft_strmapi.c \
-					ft_strcat.c \
 					ft_strjoin.c \
 					ft_strlcpy.c \
-					ft_strndup.c \
 					ft_split.c \
 					ft_atoi.c \
 					ft_strlcat.c \
-					ft_itoa.c)
-MEM_SRCS_NAME	= $(addprefix $(MEM_SRCS_PATH)/, ft_bzero.c \
-					ft_memalloc.c \
+					ft_itoa.c \
+					ft_bzero.c \
 					ft_memccpy.c \
 					ft_memchr.c \
 					ft_memcmp.c \
 					ft_memcpy.c \
-					ft_memdel.c \
 					ft_memmove.c \
 					ft_memset.c \
-					ft_calloc.c)
-CHAR_SRCS_NAME	= $(addprefix $(CHAR_SRCS_PATH)/, ft_tolower.c \
+					ft_calloc.c \
+					ft_tolower.c \
 					ft_isdigit.c \
-					ft_islower.c \
-					ft_isupper.c \
 					ft_isalpha.c \
 					ft_isalnum.c \
 					ft_isascii.c \
 					ft_tolower.c \
 					ft_isprint.c \
-					ft_toupper.c)
-FD_SRCS_NAME	= $(addprefix $(FD_SRCS_PATH)/, ft_putchar_fd.c \
-					 ft_putstr_fd.c \
-					 ft_putendl_fd.c \
-					 ft_putnbr_fd.c)
-BONUS_SRCS_NAME	= $(addprefix $(BONUS_SRCS_PATH)/, ft_lstnew.c \
+					ft_toupper.c \
+					ft_putchar_fd.c \
+					ft_putstr_fd.c \
+					ft_putendl_fd.c \
+					ft_putnbr_fd.c)
+BONUS			= $(addprefix $(SRCS_PATH)/, ft_lstnew.c \
 					ft_lstadd_front.c \
 					ft_lstsize.c \
 					ft_lstlast.c \
@@ -74,14 +60,6 @@ BONUS_SRCS_NAME	= $(addprefix $(BONUS_SRCS_PATH)/, ft_lstnew.c \
 					ft_lstclear.c \
 					ft_lstiter.c \
 					ft_lstmap.c)
-INCS_NAME		= libft.h
-
-INCS			= $(addprefix $(INCS_PATH)/, $(INCS_NAME))
-SRCS			= $(addprefix $(SRCS_PATH)/, $(STR_SRCS_NAME) \
-					$(MEM_SRCS_NAME) \
-					$(CHAR_SRCS_NAME) \
-					$(FD_SRCS_NAME))
-BONUS			= $(addprefix $(SRCS_PATH)/, $(BONUS_SRCS_NAME))
 
 OBJS			= $(SRCS:.c=.o)
 BONUS_OBJS		= $(BONUS:.c=.o)
--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: jpeyron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 15:52:47 by jpeyron           #+#    #+#             */
-/*   Updated: 2020/11/18 18:51:36 by jpeyron          ###   ########.fr       */
+/*   Updated: 2020/11/19 20:23:46 by jpeyron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,39 +24,23 @@ void			*ft_memmove(void *dest, const void *src, size_t n);
 void			*ft_memset(void *s, int c, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
-void			*ft_memalloc(size_t size);
-void			ft_memdel(void **ap);
 void			*ft_calloc(size_t nmenb, size_t size);
 int				ft_atoi(const char *str);
 char			*ft_itoa(int n);
-size_t			ft_strlcat(char *dest, char *src, size_t size);
-char			*ft_strncpy(char *dest, char *src, size_t len);
-void			ft_strdel(char **as);
+size_t			ft_strlcat(char *dest, const char *src, size_t size);
 char			*ft_strdup(const char *s);
-int				ft_strnequ(const char *s1, const char *s2, size_t n);
-int				ft_strequ(const char *s1, const char *s2);
 char			*ft_substr(const char *s, unsigned int start, size_t len);
-char			*ft_strnew(size_t size);
 size_t			ft_strlen(const char *s);
-char			*ft_strcpy(char *dest, char *src);
 char			*ft_strtrim(const char *s, char const *se);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strnstr(const char *str, const char *to_find, size_t len);
-void			ft_strclr(char *s);
-void			ft_striter(char *s, void (*f)(char *));
-void			ft_striteri(char *s, void (*f)(unsigned int, char *));
-char			*ft_strndup(const char *s, size_t n);
-char			*ft_strmap(char const *s, char (*f)(char));
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char			*ft_strcat(char *dest, const char *src);
 char			*ft_strjoin(char const *s1, char const *s2);
 size_t			ft_strlcpy(char *dest, char const *src, size_t size);
 char			**ft_split(char const *s, char c);
 int				ft_tolower(int c);
-int				ft_islower(int c);
-int				ft_isupper(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
